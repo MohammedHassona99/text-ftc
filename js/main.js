872511fafafbc9fc6001d2f75f5e51f1afdea2db
MohammedHassona99 <mohammedhassona99@hotmail.com>
2021-07-11 17:53:18 +0300

    all-sections

---
+++ js/main.js
@@ -0,0 +1,7 @@
+var typed = new Typed("#para", {
+  strings: [
+    `Testing real world engineering skills: (Code Comprehension, Debugging, Contribution, System Design, ...)`,
+  ],
+  typeSpeed: 50,
+  loop: true,
+});
